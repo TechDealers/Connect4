1b1cbfb9519e6cf8fdbc68299413dc69ff14da2b
MurdeRM3L0DY <>
2023-04-28 11:19:37 +0200

    feat(F4Client): add drawBoard function

---
+++ include/F4Client.h
@@ -0,0 +1,1 @@
+void drawBoard(char **, int, int);
--- src/F4Client.c
+++ src/F4Client.c
@@ -1,2 +1,39 @@
-int main(int argc, char **argv) {
+#include "F4Client.h"
+#include <stdio.h>
+
+void drawBoard(char **B, int rows, int cols) {
+    // top border
+    printf("┌");
+    for (int i = 0; i < cols - 1; ++i) {
+        printf("─┬");
+    }
+    printf("─┐\n");
+
+    // player symbols
+    for (int i = 0; i < rows; ++i) {
+        // left border
+        printf("│");
+        for (int j = 0; j < cols; ++j) {
+            printf("%c│", B[i][j]);
+        }
+
+        if (i < rows - 1) {
+            printf("\n├");
+            for (int j = 0; j < cols - 1; ++j) {
+                printf("─┼");
+            }
+            // right border
+            printf("─┤");
+        }
+        printf("\n");
+    }
+
+    // bottom border
+    printf("└");
+    for (int i = 0; i < cols - 1; ++i) {
+        printf("─┴");
+    }
+    printf("─┘\n");
 }
+
+int main(int argc, char **argv) {}
